a3b8092d89cdea7fb14176a9d6a0eacfb65e2ea1
Kaushik Nagaraj <kaushikstan@gmail.com>
2021-11-29 23:01:52 -0500

    Updates

---
+++ precache-manifest.ae118ecb471c791bb292b5879a1466ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5f24f3afcfbde4e2c10d7dd23e4c04",
+    "url": "/crypto-donater/index.html"
+  },
+  {
+    "revision": "f88b6cd34cdbda76a1d1",
+    "url": "/crypto-donater/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c1bb544951ac8e3e1092",
+    "url": "/crypto-donater/static/js/2.0cce5b5d.chunk.js"
+  },
+  {
+    "revision": "f88b6cd34cdbda76a1d1",
+    "url": "/crypto-donater/static/js/main.0b8e7829.chunk.js"
+  },
+  {
+    "revision": "2a2f7c1d7f0e1012dce6",
+    "url": "/crypto-donater/static/js/runtime-main.a74fb9d6.js"
+  }
+]);
